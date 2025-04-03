a862526f7999834fb1f26af98f1a1593ccc31b57
Roland Laboulaye <rlaboulaye@gmail.com>
2025-04-02 22:11:52 -0400

    cleaned imports

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod app;
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-    use crate::app::*;
+    use crate::app::App;
     tracing_wasm::set_as_global_default();
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App);
