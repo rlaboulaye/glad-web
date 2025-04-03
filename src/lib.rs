055670c4f5cbd4ffd8608f4d244aed3976546b79
Roland Laboulaye <rlaboulaye@gmail.com>
2025-04-02 22:10:55 -0400

    added js functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@ pub mod app;
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use crate::app::*;
+    tracing_wasm::set_as_global_default();
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App);
+}
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen(module = "/js/utils.js")]
+extern "C" {
+    fn decodeJWT(token: String) -> String;
+    fn emailRegex(email: &str) -> bool;
 }
