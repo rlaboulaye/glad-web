4a919e13e7d7227235c35cd4124071f7db0a0919
Roland Laboulaye <rlaboulaye@gmail.com>
2025-05-22 16:56:15 -0400

    menu hiding sans mobile logo

--- src/components/navitems.rs
+++ src/components/navitems.rs
@@ -159,84 +159,81 @@ pub(crate) fn NavItems(logout: LogoutSignal, username: UsernameSignal) -> impl I
 
     view! {
         <nav class="bg-gray-800">
-            // <button
-            //     type="button"
-            //     on:click= move |_| toggle_menu(MenuID::Mobile)
-            // >
-            //     "Another Button"
-            // </button>
             <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                // <button
-                //     type="button"
-                //     on:click= move |_| toggle_menu(MenuID::Mobile)
-                // >
-                //     "Another Button"
-                // </button>
                 <div class="relative flex h-16 items-center justify-between">
-                    // <button
-                    //     type="button"
-                    //     on:click= move |_| toggle_menu(MenuID::Mobile)
-                    // >
-                    //     "Another Button"
-                    // </button>
                     <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         <button
                             type="button"
                             on:click= move |_| toggle_menu(MenuID::Mobile)
                         >
-                            "Another Button"
+                            "BUTTON"
+                            // <svg
+                            //     class="block size-6"
+                            //     fill="none"
+                            //     viewBox="0 0 24 24"
+                            //     stroke-width="1.5"
+                            //     stroke="currentColor"
+                            //     aria-hidden="true"
+                            //     data-slot="icon"
+                            // >
+                            //     <path
+                            //         stroke-linecap="round"
+                            //         stroke-linejoin="round"
+                            //         d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                            //     />
+                            // </svg>
                         </button>
                         // <!-- Mobile menu button-->
-                        <button
-                            type="button"
-                            // on:click= move |_| toggle_menu(MenuID::Mobile)
-                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                            aria-controls="mobile-menu"
-                            aria-expanded="false"
-                        >
-                            <span class="absolute -inset-0.5"></span>
-                            <span class="sr-only">Open main menu</span>
-                            // <!--
-                            // Icon when menu is closed.
-                            //
-                            // Menu open: "hidden", Menu closed: "block"
-                            // -->
-                            <svg
-                                class="block size-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                                data-slot="icon"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                />
-                            </svg>
-                            // <!--
-                            // Icon when menu is open.
-                            //
-                            // Menu open: "block", Menu closed: "hidden"
-                            // -->
-                            <svg
-                                class="hidden size-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                                data-slot="icon"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M6 18 18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
+                        // <button
+                        //     type="button"
+                        //     // on:click= move |_| toggle_menu(MenuID::Mobile)
+                        //     class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        //     aria-controls="mobile-menu"
+                        //     aria-expanded="false"
+                        // >
+                        //     <span class="absolute -inset-0.5"></span>
+                        //     <span class="sr-only">Open main menu</span>
+                        //     // <!--
+                        //     // Icon when menu is closed.
+                        //     //
+                        //     // Menu open: "hidden", Menu closed: "block"
+                        //     // -->
+                        //     <svg
+                        //         class="block size-6"
+                        //         fill="none"
+                        //         viewBox="0 0 24 24"
+                        //         stroke-width="1.5"
+                        //         stroke="currentColor"
+                        //         aria-hidden="true"
+                        //         data-slot="icon"
+                        //     >
+                        //         <path
+                        //             stroke-linecap="round"
+                        //             stroke-linejoin="round"
+                        //             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        //         />
+                        //     </svg>
+                        //     // <!--
+                        //     // Icon when menu is open.
+                        //     //
+                        //     // Menu open: "block", Menu closed: "hidden"
+                        //     // -->
+                        //     <svg
+                        //         class="hidden size-6"
+                        //         fill="none"
+                        //         viewBox="0 0 24 24"
+                        //         stroke-width="1.5"
+                        //         stroke="currentColor"
+                        //         aria-hidden="true"
+                        //         data-slot="icon"
+                        //     >
+                        //         <path
+                        //             stroke-linecap="round"
+                        //             stroke-linejoin="round"
+                        //             d="M6 18 18 6M6 6l12 12"
+                        //         />
+                        //     </svg>
+                        // </button>
                     </div>
                     <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div class="flex shrink-0 items-center">
